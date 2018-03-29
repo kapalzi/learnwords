fa7bed69c2ab4a693e5a240f030d5254890e4eb0
Krzysztof Kapała <krzysztof@macbook-pro-krzysztof.home>
2018-03-29 21:11:49 +0200

    added settings class

---
+++ learnwords/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  learnwords
+//
+//  Created by Krzysztof Kapała on 29.03.2018.
+//  Copyright © 2018 kapala. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+}
